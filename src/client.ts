73eaecf2d3646e439b91baa50466d29faffaeb80
sargonpiraev <sargonpiraev@gmail.com>
2025-07-01 14:51:27 +0300

    chore: update @sargonpiraev/habitify-api-client to version 1.7.0

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,5 @@
 import { HabitifyApiClient } from '@sargonpiraev/habitify-api-client'
 import { env } from './env.js'
-import { logger } from './server.js'
 
 // Create Habitify API client
-export const habitifyClient = new HabitifyApiClient(env.HABITIFY_API_KEY, logger)
+export const habitifyClient = new HabitifyApiClient(env.HABITIFY_API_KEY)
